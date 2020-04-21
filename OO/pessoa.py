266eb0e641bb80f93be888a417e02a7d6f4813de
philipaps <philipalvesline@gmail.com>
2020-04-21 09:46:37 -0300

    Criado atributos de instância

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,10 +1,17 @@
 class Pessoa:
+    def __init__(self,nome=None,idade=35):#execultado quando se constroi o objeto
+        #self.nome=None#existe mas sem valor
+        self.nome=nome#passado o valor do parametro
+        self.idade=idade
+
     def cumprimentar(self):
-        return 'Ola'
+        return f'Ola {id(self)}'
 
 
 if __name__=='__main__':
-    p=Pessoa()
+    p=Pessoa('Philip')
     print(Pessoa.cumprimentar(p))#sem necessidade
     print(id(p))
     print(p.cumprimentar())#forma mais usual,o self será passado automaticamente
+    p.nome='Philip'
+    print(p.nome)
