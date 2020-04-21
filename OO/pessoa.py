1ea7e984445c69ce368df1c4e882d49580fa842c
philipaps <philipalvesline@gmail.com>
2020-04-21 10:02:08 -0300

    Criado atributos complexo

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,17 +1,21 @@
 class Pessoa:
-    def __init__(self,nome=None,idade=35):#execultado quando se constroi o objeto
+    def __init__(self, *filhos , nome=None, idade=35):#execultado quando se constroi o objeto
         #self.nome=None#existe mas sem valor
         self.nome=nome#passado o valor do parametro
         self.idade=idade
+        self.filhos=list(filhos)
 
     def cumprimentar(self):
         return f'Ola {id(self)}'
 
 
 if __name__=='__main__':
-    p=Pessoa('Philip')
-    print(Pessoa.cumprimentar(p))#sem necessidade
-    print(id(p))
-    print(p.cumprimentar())#forma mais usual,o self será passado automaticamente
-    p.nome='Philip'
-    print(p.nome)
+    alves=Pessoa(nome='Alves')
+    philip=Pessoa(alves,nome='Philip')#passando alves como atributo filho
+    print(Pessoa.cumprimentar(philip))#sem necessidade
+    print(id(philip))
+    print(philip.cumprimentar())#forma mais usual,o self será passado automaticamente
+    print(philip.nome)
+    print(philip.idade)
+    for filho in philip.filhos:
+        print(filho.nome)
