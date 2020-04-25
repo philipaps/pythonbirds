05f3a8753ba9d17eee6118744de624e651b37e27
philipaps <philipalvesline@gmail.com>
2020-04-25 20:30:38 -0300

    Aplicado sobrescrita de metodos

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -7,16 +7,23 @@ class Pessoa:
         self.filhos=list(filhos)
 
     def cumprimentar(self):
-        return f'Ola {id(self)}'
+        return f'Ola meu nome é {self.nome}'
 
 class Homem(Pessoa):
-    pass
+    def cumprimentar(self):
+        #cumprimentar_classepai=Pessoa.cumprimentar(self)#passando o atual objeto
+        #da forma mais correta
+        cumprimentar_classepai = super().cumprimentar()
+        return f'{cumprimentar_classepai}, Aperto de mão'
+
+class Mutante(Pessoa):
+    olhos=3
 
 if __name__=='__main__':
-    alves=Pessoa(nome='Alves')
+    alves=Mutante(nome='Alves')
     #herdando de pessoa como alternativa poredera ser:
     #alves = Homem(nome='Alves')
-    philip=Pessoa(alves,nome='Philip')#passando alves como atributo filho
+    philip=Homem(alves,nome='Philip')#passando alves como atributo filho
     print(Pessoa.cumprimentar(philip))#sem necessidade
     print(id(philip))
     print(philip.cumprimentar())#forma mais usual,o self será passado automaticamente
@@ -29,11 +36,14 @@ if __name__=='__main__':
     print(philip.sobrenome)#somente pra este objeto
     del philip.filhos #remove dinamicamente
     philip.olhos=1
-    del philip.olhos
+    #del philip.olhos
     print(philip.__dict__)
     print(alves.__dict__)
     print(Pessoa.olhos)#viavel pois é um atributos de classe
+    print(alves.olhos)
     print(id(Pessoa.olhos),id(philip.olhos))
     pessoa=Pessoa('anonimo')
     print(isinstance(pessoa,Pessoa))
-
+    #==========================
+    print(philip.cumprimentar())
+    print(alves.cumprimentar())
